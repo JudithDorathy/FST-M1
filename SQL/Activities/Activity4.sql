e67c869cf1adfc253fe2741f2ff2189fc81ad463
JudithDorathy <judith.dorathy@gmail.com>
2025-12-06 13:18:12 +0530

    Add files via upload

---
+++ SQL/Activities/Activity4.sql
@@ -0,0 +1,42 @@
+-- Create salesman table
+CREATE TABLE salesman (
+    salesman_id int PRIMARY KEY,
+    salesman_name varchar2(32),
+    salesman_city varchar2(32),
+    commission int
+);
+
+DESCRIBE salesman;
+
+-- Insert one row
+INSERT INTO salesman VALUES(5001, 'James Hoog', 'New York', 15);
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+-- Insert multiple rows at once
+INSERT ALL
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11)
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14)
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13)
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12)
+SELECT 1 FROM DUAL;
+
+-- View data from all columns
+SELECT * FROM salesman;
+
+-- Show data from the salesman_id and city columns
+SELECT salesman_id, salesman_city FROM salesman;
+
+-- Show data of salesman from Paris
+SELECT * FROM salesman WHERE salesman_city='Paris';
+
+-- Show salesman_id and commission of Paul Adam
+SELECT salesman_id, commission FROM salesman WHERE salesman_name='Paul Adam';
+
+-- Add the grade column
+ALTER TABLE salesman ADD grade int;
+
+-- Update the values in the grade column
+UPDATE salesman SET grade=100;
+
+-- Display data
+SELECT * FROM salesman;
