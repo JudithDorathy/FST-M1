375003da641d2c454f8216d65b83a44c3374827e
JudithDorathy <judith.dorathy@gmail.com>
2025-12-29 12:08:35 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity7n.java
@@ -0,0 +1,67 @@
+package FST_TestNG;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.AssertJUnit;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Activity7n {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeMethod
+	@BeforeClass
+    public void setUp() {
+        // Initialize driver
+        driver = new FirefoxDriver();
+        // Initialize wait
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        // Open the page
+        driver.get("https://training-support.net/webelements/login-form");
+    }
+
+    @DataProvider(name = "Credentials")
+    public static Object[][] creds() {
+        return new Object[][] { 
+            { "admin1", "password1", "Invalid Credentials" },
+            { "wrongAdmin", "wrongPassword", "Invalid Credentials" }
+        };
+    }
+
+    @Test(dataProvider = "Credentials")
+    public void loginTest(String username, String password, String expectedMessage) {
+        // Find the input fields and the login button
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Submit']"));
+
+        // Clear the input fields
+        usernameField.clear();
+        passwordField.clear();
+        // Enter the credentials and click Log in
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        loginButton.click();
+
+        // Assert login message
+        String loginMessage = driver.findElement(By.id("subheading")).getText();
+        AssertJUnit.assertEquals(loginMessage, expectedMessage);
+    }
+
+    @AfterMethod
+	@AfterClass
+    public void tearDown() {
+        // Close the browser
+        driver.quit();
+    }
+}
---
+++ TestNG/Activities/DemoTwo.java
@@ -0,0 +1,9 @@
+package FST_TestNG;
+import org.testng.annotations.Test;
+
+public class DemoTwo {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
